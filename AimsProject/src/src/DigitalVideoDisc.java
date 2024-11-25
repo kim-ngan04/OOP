3d148eabd651d7682123c04abbccd8752c5b53cb
Đặng Kim Ngân <dkimngan1411@gmail.com>
2024-11-25 19:40:23 +0700

    passing-parameter

--- AimsProject/src/src/DigitalVideoDisc.java
+++ AimsProject/src/src/DigitalVideoDisc.java
@@ -10,6 +10,10 @@ public class DigitalVideoDisc {
 	public String getTitle() {
 		return title;
 	}
+	
+	public void setTitle(String title) {
+        this.title = title;
+    }
 
 	public String getCategory() {
 		return category;
